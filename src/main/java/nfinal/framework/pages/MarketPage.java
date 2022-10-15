64c9fd6db18a9cc0fe6bd0210f6a3438ff5b1645
S1lkBeard <jahwrath@yahoo.com>
2022-10-15 21:51:44 +0300

    Изменены методы для реализации параметризованного сценария

--- src/main/java/nfinal/framework/pages/MarketPage.java
+++ src/main/java/nfinal/framework/pages/MarketPage.java
@@ -1,5 +1,7 @@
 package nfinal.framework.pages;
 
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
@@ -10,41 +12,31 @@ public class MarketPage extends BasePage {
 
     @FindBy(xpath = "//div[contains (text(), 'Маркет')]")
     public WebElement market;
-    
-    @FindBy(xpath = "//li[@data-node-id='6n2ld']")
+
+    @FindBy(xpath = "//span[contains (text(), 'Электроника')]")
     public WebElement electronics;
 
     @FindBy(xpath = "//a[@href='https://market.yandex.ru/catalog--televizory-i-aksessuary/26960170']")
     public WebElement tvAndAcc;
 
-    @FindBy(xpath = "//a[@href='https://mark']")
+    @FindBy(xpath = "//a[@href='https://market.yandex.ru/catalog--naushniki-i-audiotekhnika/26992071']")
     public WebElement headfonesAndAudio;
-
-    @FindBy(xpath = "//a[contains (text(), 'Телевизоры')]")
-    public WebElement tv;
-
-    @FindBy(xpath = "//a[contains (text(), 'Наушники')]")
-    public WebElement headfonesAndBluetooth;
-
     @FindBy(xpath = "//span[contains (text(), 'Все фильтры')]")
     public WebElement allFilters;
 
     @FindBy(xpath = "/html/body/div[4]/section/div[2]/div/div/div[2]/div[1]/div[1]/div/div/div/div[1]/input")
     public WebElement startPrice;
 
-    @FindBy(xpath = "//input[@value='Samsung']")
+    @FindBy(xpath = "//label[@id='153061']")
     public WebElement makerSamsung;
 
     @FindBy(xpath = "//input[@value='LG']")
     public WebElement makerLG;
 
-    @FindBy(xpath = "//input[@value='JBL']")
-    public WebElement makerJBL;
-
     @FindBy(xpath = "//a[contains (text(), 'Показать')]")
     public WebElement showResults;
 
-    @FindBy(xpath = "//div/div[@class='_2im8- _2S9MU _2jRxX']")
+    @FindBy(xpath = "//img[@class='_2UO7K']")
     public WebElement firstResult;
 
     @FindBy(xpath = "//input[@id='header-search']")
@@ -53,7 +45,11 @@ public class MarketPage extends BasePage {
     @FindBy(xpath = "//button[@data-r='search-button']")
     public WebElement buttonFind;
 
-    public String result;
+    @FindBy(xpath = "//img[@class='_2UO7K']")
+    public WebElement resultForComparison;
+
+    public String resultFirst;
+    public String resultSecond;
 
     public MarketPage clickServices() {
         services.click();
@@ -62,27 +58,28 @@ public class MarketPage extends BasePage {
     }
 
     public MarketPage choiseElectronics() {
+        for (String newWindow : driverManager.getDriver().getWindowHandles()) {
+            driverManager.getDriver().switchTo().window(newWindow);
+        }
         electronics.click();
         return pageManager.getMarketPage();
     }
 
-    public MarketPage choiseTvAndAccCat() {
-        tvAndAcc.click();
+    public MarketPage choiseCat(String name) {
+        switch (name) {
+            case "Телевизоры и аксессуары":
+                tvAndAcc.click();
+                break;
+            case "Наушники и аудиотехника":
+                headfonesAndAudio.click();
+                break;
+        }
         return pageManager.getMarketPage();
     }
 
-    public MarketPage choiseTvCat() {
-        tv.click();
-        return pageManager.getMarketPage();
-    }
-
-    public MarketPage choiseHeadfonesAndAudio() {
-        headfonesAndAudio.click();
-        return pageManager.getMarketPage();
-    }
-
-    public MarketPage choiseHeadfonesAndBluetooth() {
-        headfonesAndBluetooth.click();
+    public MarketPage choiseSubCat(String name) {
+        WebElement element = driverManager.getDriver().findElement(By.partialLinkText(name));
+        element.click();
         return pageManager.getMarketPage();
     }
 
@@ -98,14 +95,18 @@ public class MarketPage extends BasePage {
         return pageManager.getMarketPage();
     }
 
-    public MarketPage setMakers() {
-        makerSamsung.click();
-        makerLG.click();
-        return pageManager.getMarketPage();
-    }
-
-    public MarketPage setMakersJBL() {
-        makerJBL.click();
+    public MarketPage setMakers(String makers) {
+        switch (makers) {
+            case "Samsung и LG":
+                js.executeScript("arguments[0].click()", makerSamsung);
+                js.executeScript("arguments[0].click()", makerLG);
+                break;
+            case "JBL":
+                WebElement element = driverManager.getDriver().findElement
+                        (By.xpath("//input[@value='"+makers+"']"));
+                js.executeScript("arguments[0].click()", element);
+                break;
+        }
         return pageManager.getMarketPage();
     }
 
@@ -115,14 +116,20 @@ public class MarketPage extends BasePage {
     }
 
     public MarketPage choiseFirstResult() {
-        result = firstResult.getText();
+        resultFirst = firstResult.getAttribute("alt");
         return pageManager.getMarketPage();
     }
 
     public MarketPage clickSearchBar() {
         searchBar.click();
-        searchBar.sendKeys(result);
+        searchBar.sendKeys(resultFirst);
         buttonFind.click();
+        return pageManager.getMarketPage();
+    }
+
+    public MarketPage checkFoundResult() {
+        resultSecond = resultForComparison.getAttribute("alt");
+        Assert.assertTrue("Результат поиска не совпал", resultSecond.equals(resultFirst));
         return pageManager.getMarketPage();
     }
 }
